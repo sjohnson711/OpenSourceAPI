8cface7e5fe73d79bb91813293dcf1163fb06118
Seth <samaritanbrotherseth@gmail.com>
2025-05-25 21:58:12 -0400

    just added new html to link that will give a description of the coffees in English for most

--- JS/index.js
+++ JS/index.js
@@ -1,40 +1,40 @@
 
-async function getRandomCoffee(type){
-    const url1 = `https://api.sampleapis.com/coffee/${type}`
+async function getRandomCoffee(type){ 
+    const url1 = `https://api.sampleapis.com/coffee/${type}` 
     try { 
         const resp = await fetch(url1); //awaiting fetch call from API
         const coffees = await resp.json(); //receiving data from API
 
-        if(!Array.isArray(coffees) || coffees.length === 0){
+        if(coffees.length === 0){
             throw new Error("No coffee data available at this moment due to the server")
         }
 
-        const randomIndex = Math.floor(Math.random()* coffees.length);
-        const coffee = coffees[randomIndex];
+        const randomIndex = Math.floor(Math.random()* coffees.length); //random index of coffee in the api resp.json()
+        const coffee = coffees[randomIndex]; //using randomIndex to give a randomcoffee
 
-        displayCoffee(coffee); //displayCoffee
+        displayCoffee(coffee); 
 
     }catch(error) {
-        document.getElementById('coffeeDisplay').innerText = "Error: " + error.message;
+        document.getElementById('coffeeDisplay').innerText = "Error: " + error.message; 
     }
 }
 
 
 //creating function to display coffee details on the page
 function displayCoffee(coffee){
-    const container = document.getElementById('coffeeDisplay');
-    container.innerHTML =  
-        `<h2>${coffee.title}</h2>
+    const container = document.getElementById('coffeeDisplay'); //using the div to display the random coffee
+    container.innerHTML =  //used to create different tags in the container to display the information
+        `<h2>${coffee.title}</h2> 
         <img src="${coffee.image}" alt="${coffee.title}" width="200"/>
-        <p>${coffee.description}</p>
+        <p id="coffee-description">${coffee.description}</p>
     `;
 }
 
-// Add event listeners to buttons
+// Add event listeners to buttons to call function and make sure button is linked to the type that we will be requesting from the API
 document.getElementById('hotButton').addEventListener('click', () => {
   getRandomCoffee('hot');
 });
 
 document.getElementById('coldButton').addEventListener('click', () => {
-  getRandomCoffee('cold');
+  getRandomCoffee('iced'); //corrected from cold to iced
 });
